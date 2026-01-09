376c2954412b6a0bc0d7f87edf6f8f29dec2e28e
Abubakir <abubakrbohodirov185@gmail.com>
2026-01-09 16:50:45 +0500

    Remove hardcoded SECRET_KEY from settings
    Removed the hardcoded SECRET_KEY value for security reasons.

--- config/settings.py
+++ config/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m0_1$vf+5fy^3j=yx&%&v(okh8$ti8-9w5f7orf4ddp@_^lvkp'
-
+SECRET_KEY =
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
